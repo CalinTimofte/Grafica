fea5683665a9e00e557df80ff547ee448f33c1a2
platicaalex <platicaalex@gmail.com>
2020-03-09 15:49:38 +0200

    update main principal

--- Lab2/lab2/main.cpp
+++ Lab2/lab2/main.cpp
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
+#include <iostream>
 #include "glut.h"
 
 // dimensiunea ferestrei in pixeli
@@ -79,7 +79,7 @@ void Display1() {
 void Display2() {
 	double pi = 4 * atan(1.0);
 	double xmax = 8 * pi;
-	double ymax = exp(1.1);
+	double ymax = exp(1);
 	double ratia = 0.05;
 	
 	// afisarea punctelor propriu-zise precedata de scalare
@@ -95,6 +95,155 @@ void Display2() {
 	glEnd();
 }
 
+//Functia f(x)
+void Display3() {
+	//to do: xmax si ymax
+
+	double ratia = 0.05;
+	double xmax = 20;
+	double ymax = 1;
+
+		glColor3f(1, 0.1, 0.1);
+		glBegin(GL_LINE_STRIP);
+		for (double x = 0; x <= 100; x += ratia) {
+			double y;
+			if (x == 0) {
+				y = 1;
+			}
+			else {
+				y = (x - int(x)) / x;
+			}
+			double x1 = x / xmax;
+			glVertex2f(x1, y);
+		}
+		glEnd();
+	}
+
+//melcul lui Pascal
+void Display4() {
+	//to do: umplut spatiu intre cele doua curbe
+	//to do: xmax si ymax
+
+	double pi = 4 * atan(1.0);
+	double ratia = 0.05;
+	double a = 0.3, b = 0.2;
+	double xmax = pi + ratia;
+
+	glColor3f(1, 0.1, 0.1);
+	glBegin(GL_LINE_STRIP);
+	for (double t = -pi+ratia; t < pi; t += ratia) { 
+		double x = 2 * (a * cos(t) + b) * cos(t);
+		double y = 2 * (a * cos(t) + b) * sin(t);
+		glVertex2f(x, y);
+	}
+	glEnd();
+}
+
+//Trisectoarea lui Longchamps
+void Display5() {
+
+}
+
+//Cicloida
+void Display6() {
+	//to do maybe?? xmax ymax, why 3*pi and -3 * pi??
+
+	double pi = 4 * atan(1.0);
+	double ratia = 0.05;
+	double a = 0.1, b = 0.2;
+
+	glColor3f(1, 0.1, 0.1);
+	glBegin(GL_LINE_STRIP);
+
+	for (double t = -3*pi;t<=3*pi ; t += ratia) {
+		double x = a * t - b * sin(t);
+		double y = a - b * cos(t);
+
+		glVertex2f(x, y);
+	}
+
+	glEnd();
+}
+
+void Display7() {
+	//done, check it for eventual revising
+
+	double pi = 4 * atan(1.0);
+	double ratia = 0.05;
+	double R = 0.1, r = 0.3;
+
+	glColor3f(1, 0.1, 0.1);
+	glBegin(GL_LINE_STRIP);
+	for (double t = 0; t <= 2 * pi; t += ratia) {
+		double x = (R + r) * cos(r / R * t) - r * cos(t + r / R * t);
+		double y = (R + r) * sin(r / R * t) - r * sin(t + r / R * t);
+
+		glVertex2f(x, y);
+	}
+	glEnd();
+}
+
+//Hipocicloida
+void Display8() {
+	//done, check it for eventual revising
+
+	double pi = 4 * atan(1.0);
+	double ratia = 0.05;
+	double R = 0.1, r = 0.3;
+
+	glColor3f(1, 0.1, 0.1);
+	glBegin(GL_LINE_STRIP);
+	for (double t = 0; t <= 2 * pi; t += ratia) {
+		double x = (R - r) * cos(r / R * t) - r * cos(t - r / R * t);
+		double y = (R - r) * sin(r / R * t) - r * sin(t - r / R * t);
+
+		glVertex2f(x, y);
+	}
+	glEnd();
+}
+
+//Lemniscata lui Bernoulli
+void Display9() {
+	//done, check it for eventual revising 
+
+	double pi = 4 * atan(1.0);
+	double ratia = 0.001;
+	double a = 0.4;
+
+	glColor3f(1, 0.1, 0.1);
+	glBegin(GL_LINE_STRIP);
+	for (double t = (-pi) / 4 + ratia; t < pi / 4; t += ratia) {
+		double x1 = (a * sqrt(2 * cos(2 * t))) * cos(t);
+		double y1 = (a * sqrt(2 * cos(2 * t))) * sin(t);
+		
+		glVertex2f(x1, -y1);
+	}
+	for (double t = (-pi) / 4 + ratia; t < pi / 4; t += ratia) {
+		double x2 = (-a * sqrt(2 * cos(2 * t))) * cos(t);
+		double y2 = (-a * sqrt(2 * cos(2 * t))) * sin(t);
+		
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+}
+
+//Spirala logaritmica
+void Display10() {
+	//am hardcodat limita maxima din for, nu cred ca e corect
+
+	double ratia = 0.05;
+	double a = 0.02;
+
+	glColor3f(1, 0.1, 0.1);
+	glBegin(GL_LINE_STRIP);
+	for (double t = ratia; t < 2.9; t += ratia) {
+		double x = a * exp(1 + t) * cos(t);
+		double y = a * exp(1 + t) * sin(t);
+
+		glVertex2f(x, y);
+	}
+	glEnd();
+}
 
 void Init(void) {
 
@@ -116,6 +265,30 @@ void Display(void) {
 		break;
 	case '2':
 		Display2();
+		break;
+	case '3':
+		Display3();
+		break;
+	case '4':
+		Display4();
+		break;
+	case '5':
+		Display5();
+		break;
+	case '6':
+		Display6();
+		break;
+	case '7':
+		Display7();
+		break;
+	case '8':
+		Display8();
+		break;
+	case '9':
+		Display9();
+		break;
+	case '0':
+		Display10();
 		break;
 	default:
 		break;
